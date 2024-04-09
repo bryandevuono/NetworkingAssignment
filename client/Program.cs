2b107e57b294c9b3cd15c93be1704c9aed44d495
JulinaMay <julinamay_@Hotmail.com>
2024-04-09 13:36:09 +0200

    First run

--- client/Program.cs
+++ client/Program.cs
@@ -18,14 +18,38 @@ class Program
 class ClientUDP
 {
 
+    private Socket _socket;
+    private IPEndPoint _serverEndPoint;
+
     //TODO: implement all necessary logic to create sockets and handle incoming messages
     // Do not put all the logic into one method. Create multiple methods to handle different tasks.
     public void start()
     {
-
+        _serverEndPoint = GetServerEndPoint();
+        _socket = CreateSocket();
+        SendHelloMessage();
     }
-    //TODO: create all needed objects for your sockets 
 
+    private Socket CreateSocket()
+    {
+        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        return socket;
+    }
+
+    private IPEndPoint GetServerEndPoint()
+    {
+        // TODO: Implement the logic to get the server end point.
+        // Replace the following line with your implementation.
+        return new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+    }
+
+    //TODO: create all needed objects for your sockets 
+    private void SendHelloMessage()
+    {
+        string message = "Hello";
+        byte[] buffer = Encoding.UTF8.GetBytes(message + Environment.NewLine);
+        _socket.SendTo(buffer, _serverEndPoint);
+    }
     //TODO: [Send Hello message]
 
     //TODO: [Receive Welcome]
--- server/Program.cs
+++ server/Program.cs
@@ -7,7 +7,8 @@ using System.Text;
 using System.Text.Json;
 using MessageNS;
 
-
+// Julina Mercera | 1055662
+// Bryan de Vuono | 1061043
 // Do not modify this class
 class Program
 {
@@ -23,18 +24,45 @@ class ServerUDP
 
     //TODO: implement all necessary logic to create sockets and handle incoming messages
     // Do not put all the logic into one method. Create multiple methods to handle different tasks.
+    private Socket _socket;
+
     public void start()
     {
-
+        _socket = CreateSocket();
+        BindSocket(_socket);
+        ReceiveHelloMessage();
     }
 
     //TODO: create all needed objects for your sockets 
-
+    private Socket CreateSocket()
+    {
+        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        return socket;
+    }
     //TODO: keep receiving messages from clients
     // you can call a dedicated method to handle each received type of messages
-
+    private void BindSocket(Socket socket)
+    {
+        int portNumber = 1234; // Replace 1234 with the actual port number you want to use
+        socket.Bind(new IPEndPoint(IPAddress.Any, portNumber));
+    }
     //TODO: [Receive Hello]
+    private void ReceiveHelloMessage()
+    {
+        byte[] buffer = new byte[1024];
+        int bytesReceived = _socket.Receive(buffer);
+        string message = Encoding.UTF8.GetString(buffer, 0, bytesReceived);
 
+        if (message.Trim() == "Hello")
+        {
+            Console.WriteLine("Client says: Hello");
+        }
+        else
+        {
+            Console.WriteLine("Unexpected message: " + message);
+        }
+    }
+    // Which should print hello!
     //TODO: [Send Welcome]
 
     //TODO: [Receive RequestData]
